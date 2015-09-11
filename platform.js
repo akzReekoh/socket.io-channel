54285f75c19a1a7d33dca11c733fa1dbcac0c2cf
Benj Sicam <bsicam@reekoh.com>
2015-09-11 14:38:27 +0800

    Added notifyReady call
    The plugin should call the notifyReady event in order to notify the
    parent process that initialization is done.

--- platform.js
+++ platform.js
@@ -34,6 +34,19 @@ Platform.init = function () {
 	});
 };
 
+Platform.prototype.notifyReady = function (callback) {
+	callback = callback || function () {
+		};
+
+	setImmediate(function () {
+		process.send({
+			type: 'ready'
+		});
+
+		callback();
+	});
+};
+
 Platform.prototype.sendMessageToDevice = function (device, message, callback) {
 	callback = callback || function () {
 		};
